bf4650408958dd3043c3d002c026e7a776955d95
Kamlesh Badgujar <kamleshbadgujar00@gmail.com>
2020-06-14 19:35:37 +0530

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,61 @@
+import { Component } from '@angular/core';
+import { NullValidationHandler, OAuthService, AuthConfig } from 'angular-oauth2-oidc';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Tour of Heroes';
+  public claims: any;
+  public hasValidAccessToken: boolean = false;
+  constructor(private oauthService: OAuthService) {
+    this.configure();
+    //this.setupAutomaticSilentRefresh();
+  }
+
+  authConfig: AuthConfig = {
+    issuer: 'http://127.0.0.1:8080/auth/realms/heroes',
+    redirectUri: window.location.origin + "/heroes",
+    clientId: 'spa-heroes',
+    //dummyClientSecret: '2ea5dc66-8e1c-4e7c-aac0-52a42594a6ac',
+    scope: 'openid profile email offline_access heroes',
+    responseType: 'code',
+    requireHttps: false,
+    // at_hash is not present in JWT token
+    disableAtHashCheck: true,
+    //postLogoutRedirectUri: window.location.origin + "/heroes",
+    showDebugInformation: true
+  }
+  
+  public login() {
+    this.oauthService.initLoginFlow();
+  }
+  
+  public logoff() {
+    this.oauthService.logOut();
+  }
+  
+  private configure() {
+    this.oauthService.configure(this.authConfig);
+    this.oauthService.tokenValidationHandler = new NullValidationHandler();
+    this.oauthService.loadDiscoveryDocumentAndTryLogin();
+  }
+
+//   private setupAutomaticSilentRefresh() {
+//     this.oauthService.setupAutomaticSilentRefresh();
+//   }
+
+//   public name() {
+//      this.claims = this.oauthService.getIdentityClaims();
+//      console.log(this.claims);
+//     if (!this.claims) return null;
+//     return this.claims;
+// }
+
+// public validAccessToken() {
+//     this.hasValidAccessToken = this.oauthService.hasValidAccessToken();
+//     console.log(this.hasValidAccessToken);
+// }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,39 @@
+import { NgModule }       from '@angular/core';
+import { BrowserModule }  from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpClientModule }    from '@angular/common/http';
+import { InMemoryDataService }  from './in-memory-data.service';
+import { OAuthModule } from 'angular-oauth2-oidc';
+import { AppRoutingModule }     from './app-routing.module';
+
+import { AppComponent }         from './app.component';
+import { DashboardComponent }   from './dashboard/dashboard.component';
+import { HeroDetailComponent }  from './hero-detail/hero-detail.component';
+import { HeroesComponent }      from './heroes/heroes.component';
+import { HeroSearchComponent }  from './hero-search/hero-search.component';
+import { MessagesComponent }    from './messages/messages.component';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    OAuthModule.forRoot({
+      resourceServer: {
+          allowedUrls: ['http://localhost:9090/api'],
+          sendAccessToken: true
+      }
+  })
+  ],
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    HeroesComponent,
+    HeroDetailComponent,
+    MessagesComponent,
+    HeroSearchComponent
+  ],
+  bootstrap: [ AppComponent ]
+})
+export class AppModule { }
